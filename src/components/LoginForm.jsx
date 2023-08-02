a6b1efcaac78b88ce71493bd6479e26da1a1a2cf
BoubaWade <117430608+BoubaWade@users.noreply.github.com>
2023-08-02 18:27:54 +0200

    ✅  F01: LOGIN > update loginForm

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -6,17 +6,17 @@ function LoginForm() {
     const onChange = (e)=>{
         setValue(e.target.value);
     }
-    const onSubmit=()=>{
-        //e.preventDefault();
-        alert(value);
-        //setValue("");
+    const onSubmit=(e)=>{
+        e.preventDefault();
+        alert(`Bonjour ${value}`);
+        setValue("");
     }
     return (
         <>
           <h1>Bienvenue chez nous !</h1>
           <h2>Connectez-vous</h2>
-          <form onSubmit={onSubmit}>
-            <input type='text'  placeholder='Entrez votre prénom...' required onChange={onChange} />
+          <form onSubmit={onSubmit} >
+            <input type='text'  value={value} placeholder='Entrez votre prénom...' required onChange={onChange} />
             <input type="submit" value="Accéder à votre espace" />
           </form> 
         </>
