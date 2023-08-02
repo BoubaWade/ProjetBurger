9d6459e527aca8fec3a5ad3780f51f8bc59c5753
BoubaWade <117430608+BoubaWade@users.noreply.github.com>
2023-08-02 18:48:56 +0200

    ♻️ F01: LOGIN > refacto LoginForm

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,26 +1,30 @@
 import { useState } from "react";
 
 function LoginForm() {
-    const [value, setValue]= useState("");
+  const [value, setValue] = useState("");
 
-    const onChange = (e)=>{
-        setValue(e.target.value);
-    }
-    const onSubmit=(e)=>{
-        e.preventDefault();
-        alert(`Bonjour ${value}`);
-        setValue("");
-    }
-    return (
-        <>
-          <h1>Bienvenue chez nous !</h1>
-          <h2>Connectez-vous</h2>
-          <form onSubmit={onSubmit} >
-            <input type='text'  value={value} placeholder='Entrez votre prénom...' required onChange={onChange} />
-            <input type="submit" value="Accéder à votre espace" />
-          </form> 
-        </>
-    );
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    alert(`Bonjour ${value}`);
+    setValue("");
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Bienvenue chez nous !</h1>
+      <h2>Connectez-vous</h2>
+      <input
+        type="text"
+        value={value}
+        placeholder="Entrez votre prénom..."
+        required
+        onChange={onChange}
+      />
+      <input type="submit" value="Accéder à votre espace" />
+    </form>
+  );
 }
 
 export default LoginForm;
